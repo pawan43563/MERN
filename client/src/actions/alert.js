1e028948e6a93bcabc3fe30cc83ca914b8982f17
Your Name <pawan.ps43563@hotmail.com>
2020-04-12 01:39:34 +0530

    pawan

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,5 +1,5 @@
 import {SET_ALERT,REMOVE_ALERT} from './types';
-import uuid from 'uuid';
+import uuid from 'uuid/v4';
 export const setAlert=(msg,alertType,timeout=5000) =>dispatch=>{
     const id=uuid.v4();
     dispatch({
