1c04a7da324d7c6044c07704985467fd020e11ed
Your Name <pawan.ps43563@hotmail.com>
2020-04-11 21:41:53 +0530

    Deploy

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express=require("express");
 const app=express()
 const connectDB=require('./config/db')
-
+const path=require("path")
 
 //connect db
 connectDB()
 
-app.get("/",function(req,res){
-    res.send("Hello World!")
-})
 
 //Middleware
 app.use(express.json({extended:false}));
@@ -19,9 +16,18 @@ app.use('/api/auth',require('./routes/api/auth'));
 app.use('/api/profile',require('./routes/api/profile'));
 app.use('/api/posts',require('./routes/api/posts'));
 
+//Serve Static assests
+if(process.env.NODE_ENV==='production'){
+    //set Static folder
+    app.use(express.static('client/build'))
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 const PORT=process.env.PORT || 5000
 
 app.listen(PORT,(req,res)=>{
-    console.log("Server Started at ",PORT)
+    console.log(`Server Started at port ${PORT}` )
 });
